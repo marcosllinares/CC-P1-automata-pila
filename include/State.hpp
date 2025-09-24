5d8c60b1fb367fa3cc72ba947c2abc24222224ad
Marcos Llinares <alu0100972443@ull.edu.es>
2025-09-24 20:44:34 +0100

    Añadido clase State

--- include/State.hpp
+++ include/State.hpp
@@ -6,6 +6,15 @@
 class Transition;
 
 class State {
-public:
+ public:
+  State(){}
+  State(std::string id) : id_(id){}
+
+  std::string GetId() const { return id_; }
+  void SetId(int id) { id_ = id; }
+
+ private:
+  std::string id_;
 };
+
 
